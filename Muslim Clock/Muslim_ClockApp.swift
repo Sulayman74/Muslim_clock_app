848b5c00fedc86d5f95e2c8a79cd8d86181d4c8e
Dev Kappsi <keohavong.sirikone@gmail.com>
2026-03-27 11:21:05 +0100

    Initial Commit

---
+++ Muslim Clock/Muslim_ClockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Muslim_ClockApp.swift
+//  Muslim Clock
+//
+//  Created by Mohamed Kanoute on 27/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct Muslim_ClockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Muslim ClockTests/Muslim_ClockTests.swift
@@ -0,0 +1,16 @@
+//
+//  Muslim_ClockTests.swift
+//  Muslim ClockTests
+//
+//  Created by Mohamed Kanoute on 27/03/2026.
+//
+
+import Testing
+
+struct Muslim_ClockTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
